2507d7dc7db5eed845ead6dc644a637a45fbb79d
William Page <williamtpage@gmail.com>
2026-02-12 17:20:00 -0800

    Internal cleanups, fixes, and refactor for code reuse

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 /*!
 Derive macros for automatically adding an implementation of
 pandora_api::Pandora<Json|Rest>ApiRequest to a struct.
@@ -20,213 +21,171 @@ encrypted, this may be overridden using the #[pandora_request(encrypted = true)]
 struct attribute.
 
 */
-// SPDX-License-Identifier: MIT
 
 #![deny(missing_docs)]
+#![allow(clippy::manual_unwrap_or_default)] // darling's #[darling(default)] triggers false positive
 extern crate proc_macro;
 
 use darling::FromDeriveInput;
 use heck::ToLowerCamelCase;
 use proc_macro::TokenStream;
-use quote::{format_ident, quote, ToTokens};
-use syn::{Generics, Ident};
+use proc_macro2::Span;
+use quote::{format_ident, quote};
+use syn::{Generics, Ident, LitStr};
 
-/// Derive macro for adding implementation of pandora_api::PandoraJsonApiRequest
-/// trait to a struct.
+/// Shared attributes parsed from `#[pandora_request(...)]` for both Json and Rest derives.
 #[derive(FromDeriveInput)]
 #[darling(attributes(pandora_request))]
-struct PandoraJsonRequest {
+struct PandoraRequest {
     ident: Ident,
     generics: Generics,
-    // Default is <StructName>Response
-    #[darling(default = "std::option::Option::default")]
+    /// Default is <StructName>Response
+    #[darling(default)]
     response_type: Option<String>,
-    // Default is "Error"
-    #[darling(default = "std::option::Option::default")]
+    /// Default is "Error"
+    #[darling(default)]
     error_type: Option<String>,
-    // Default is the output of format!("{}.{}", "<ModuleName>", "<StructName>".to_lower_camel_case())
-    #[darling(default = "std::option::Option::default")]
+    /// Default is derived from module path and struct name in lower camel case.
+    #[darling(default)]
     method_name: Option<String>,
-    #[darling(default = "std::option::Option::default")]
+    #[darling(default)]
     encrypted: Option<bool>,
 }
 
-impl ToTokens for PandoraJsonRequest {
-    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        let PandoraJsonRequest {
-            ref ident,
-            ref generics,
-            ref response_type,
-            ref error_type,
-            ref method_name,
-            ref encrypted,
-        } = *self;
+/// Distinguishes the two API styles for code generation.
+enum RequestKind {
+    /// JSON API: trait PandoraJsonApiRequest, method format "{}.{}"
+    Json,
+    /// REST API: trait PandoraRestApiRequest, method format "/api/v1/{}/{}"
+    Rest,
+}
 
-        // if no response_type was specified, we default
-        // to the Self type + "Response".
-        let final_response_type = format_ident!(
-            "{}",
-            response_type
-                .as_ref()
-                .map(|s| s.to_string())
-                .unwrap_or_else(|| format!("{ident}Response"))
-        );
-        let final_error_type = format_ident!(
-            "{}",
-            error_type
-                .as_ref()
-                .map(|s| s.to_string())
-                .unwrap_or_else(|| "Error".to_string())
-        );
+impl RequestKind {
+    fn trait_ident(&self) -> Ident {
+        match self {
+            RequestKind::Json => format_ident!("PandoraJsonApiRequest"),
+            RequestKind::Rest => format_ident!("PandoraRestApiRequest"),
+        }
+    }
 
-        let get_method_decl = if let Some(method_name) = method_name {
+    fn method_format(&self) -> &'static str {
+        match self {
+            RequestKind::Json => "{}.{}",
+            RequestKind::Rest => "/api/v1/{}/{}",
+        }
+    }
+}
+
+/// Generates the `impl Trait for Struct` block for the given request and kind.
+fn emit_impl(request: &PandoraRequest, kind: RequestKind) -> proc_macro2::TokenStream {
+    let PandoraRequest {
+        ref ident,
+        ref generics,
+        ref response_type,
+        ref error_type,
+        ref method_name,
+        ref encrypted,
+    } = *request;
+
+    let final_response_type = format_ident!(
+        "{}",
+        response_type
+            .as_ref()
+            .map(String::to_string)
+            .unwrap_or_else(|| format!("{ident}Response"))
+    );
+    let final_error_type = format_ident!(
+        "{}",
+        error_type
+            .as_ref()
+            .map(String::to_string)
+            .unwrap_or_else(|| "Error".to_string())
+    );
+
+    let get_method_decl = if let Some(ref method_name) = method_name {
+        let method_name_lit = LitStr::new(method_name, Span::call_site());
+        quote! {
+            fn get_method(&self) -> String {
+                #method_name_lit.to_string()
+            }
+        }
+    } else {
+        let lower_camel_case_method = ident.to_string().to_lower_camel_case();
+        let method_format_lit = LitStr::new(kind.method_format(), Span::call_site());
+        quote! {
+            fn get_method(&self) -> String {
+                let module_name = std::module_path!();
+                let class_name = module_name.rsplitn(2, "::").next().expect("Could not infer a valid method name since there is no current module. Must pass #[pandora_request(method_name = \"<value>\")] as part of the derive.");
+                format!(#method_format_lit, class_name, #lower_camel_case_method)
+            }
+        }
+    };
+
+    let encrypt_expr = encrypted
+        .map(|b| {
             quote! {
-                fn get_method(&self) -> String {
-                    stringify!(#method_name).to_string()
+                fn encrypt_request(&self) -> bool {
+                    #b
                 }
             }
-        } else {
-            let lower_camel_case_method = ident.to_string().to_lower_camel_case();
+        })
+        .unwrap_or_else(|| {
             quote! {
-                fn get_method(&self) -> String {
-                    let module_name = std::module_path!();
-                    let class_name = module_name.rsplitn(2, "::").next().expect("Could not infer a valid method name since there is no current module. Must pass #[pandora_request(method_name = \"<value>\")] as part of the derive.");
-                    format!("{}.{}", class_name, #lower_camel_case_method)
+                fn encrypt_request(&self) -> bool {
+                    false
                 }
-            }
-        };
-
-        let encrypt_expr = encrypted
-            .map(|b| {
-                quote! {
-                    fn encrypt_request(&self) -> bool {
-                        #b
-                    }
-                }
-            })
-            .unwrap_or_else(|| quote! {});
-
-        // If the type is generic, we need to pass that
-        // through to the impl
-        let (imp, ty, wher) = generics.split_for_impl();
-        tokens.extend(quote! {
-            impl #imp PandoraJsonApiRequest for #ident #ty #wher {
-                type Response = #final_response_type;
-                type Error = #final_error_type;
-                #encrypt_expr
-                #get_method_decl
             }
         });
+
+    let trait_ident = kind.trait_ident();
+    let (imp, ty, wher) = generics.split_for_impl();
+
+    quote! {
+        impl #imp #trait_ident for #ident #ty #wher {
+            type Response = #final_response_type;
+            type Error = #final_error_type;
+            #encrypt_expr
+            #get_method_decl
+        }
     }
+}
+
+/// Parses macro input and returns a compile error TokenStream on failure.
+fn parse_input(input: TokenStream) -> Result<syn::DeriveInput, proc_macro2::TokenStream> {
+    syn::parse(input).map_err(|e| e.to_compile_error())
+}
+
+/// Parses derive input into `PandoraRequest` and returns a compile error TokenStream on failure.
+fn parse_request(ast: &syn::DeriveInput) -> Result<PandoraRequest, proc_macro2::TokenStream> {
+    PandoraRequest::from_derive_input(ast).map_err(|e| e.write_errors())
 }
 
 /// Derive macro for adding implementation of pandora_api::PandoraJsonApiRequest
 /// trait to a struct.
 #[proc_macro_derive(PandoraJsonRequest, attributes(pandora_request))]
 pub fn derive_pandora_json_request(input: TokenStream) -> TokenStream {
-    let request = PandoraJsonRequest::from_derive_input(&syn::parse(input).unwrap())
-        .expect("Failed parsing macro input");
-    let pm2_tokens: proc_macro2::TokenStream = quote! {#request};
-    //panic!("Generated tokens: {}", pm2_tokens.to_string());
-    pm2_tokens.into()
-}
-
-/// Derive macro for adding implementation of pandora_api::PandoraRestApiRequest
-/// trait to a struct.
-#[derive(FromDeriveInput)]
-#[darling(attributes(pandora_request))]
-struct PandoraRestRequest {
-    ident: Ident,
-    generics: Generics,
-    // Default is <StructName>Response
-    #[darling(default = "std::option::Option::default")]
-    response_type: Option<String>,
-    // Default is "Error"
-    #[darling(default = "std::option::Option::default")]
-    error_type: Option<String>,
-    // Default is the output of format!("{}.{}", "<ModuleName>", "<StructName>".to_lower_camel_case())
-    #[darling(default = "std::option::Option::default")]
-    method_name: Option<String>,
-    #[darling(default = "std::option::Option::default")]
-    encrypted: Option<bool>,
-}
-
-impl ToTokens for PandoraRestRequest {
-    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        let PandoraRestRequest {
-            ref ident,
-            ref generics,
-            ref response_type,
-            ref error_type,
-            ref method_name,
-            ref encrypted,
-        } = *self;
-
-        // if no response_type was specified, we default
-        // to the Self type + "Response".
-        let final_response_type = format_ident!(
-            "{}",
-            response_type
-                .as_ref()
-                .map(|s| s.to_string())
-                .unwrap_or_else(|| format!("{ident}Response"))
-        );
-        let final_error_type = format_ident!(
-            "{}",
-            error_type
-                .as_ref()
-                .map(|s| s.to_string())
-                .unwrap_or_else(|| "Error".to_string())
-        );
-
-        let get_method_decl = if let Some(method_name) = method_name {
-            quote! {
-                fn get_method(&self) -> String {
-                    stringify!(#method_name).to_string()
-                }
-            }
-        } else {
-            let lower_camel_case_method = ident.to_string().to_lower_camel_case();
-            quote! {
-                fn get_method(&self) -> String {
-                    let module_name = std::module_path!();
-                    let class_name = module_name.rsplitn(2, "::").next().expect("Could not infer a valid method name since there is no current module. Must pass #[pandora_request(method_name = \"<value>\")] as part of the derive.");
-                    format!("/api/v1/{}/{}", class_name, #lower_camel_case_method)
-                }
-            }
-        };
-
-        let encrypt_expr = encrypted
-            .map(|b| {
-                quote! {
-                    fn encrypt_request(&self) -> bool {
-                        #b
-                    }
-                }
-            })
-            .unwrap_or_else(|| quote! {});
-
-        // If the type is generic, we need to pass that
-        // through to the impl
-        let (imp, ty, wher) = generics.split_for_impl();
-        tokens.extend(quote! {
-            impl #imp PandoraRestApiRequest for #ident #ty #wher {
-                type Response = #final_response_type;
-                type Error = #final_error_type;
-                #encrypt_expr
-                #get_method_decl
-            }
-        });
-    }
+    let ast = match parse_input(input) {
+        Ok(a) => a,
+        Err(e) => return e.into(),
+    };
+    let request = match parse_request(&ast) {
+        Ok(r) => r,
+        Err(e) => return e.into(),
+    };
+    emit_impl(&request, RequestKind::Json).into()
 }
 
 /// Derive macro for adding implementation of pandora_api::PandoraRestApiRequest
 /// trait to a struct.
 #[proc_macro_derive(PandoraRestRequest, attributes(pandora_request))]
 pub fn derive_pandora_rest_request(input: TokenStream) -> TokenStream {
-    let request = PandoraRestRequest::from_derive_input(&syn::parse(input).unwrap())
-        .expect("Failed parsing macro input");
-    let pm2_tokens: proc_macro2::TokenStream = quote! {#request};
-    //panic!("Generated tokens: {}", pm2_tokens.to_string());
-    pm2_tokens.into()
+    let ast = match parse_input(input) {
+        Ok(a) => a,
+        Err(e) => return e.into(),
+    };
+    let request = match parse_request(&ast) {
+        Ok(r) => r,
+        Err(e) => return e.into(),
+    };
+    emit_impl(&request, RequestKind::Rest).into()
 }
